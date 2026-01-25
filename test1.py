cac9d20b9f9f5ffd0a4843141789efe08f2b6eed
Rabea Sayegh <rabea@example.com>
2026-01-25 23:39:45 +0200

    CrashCours

---
+++ test1.py
@@ -0,0 +1,8 @@
+number = 20
+def numberqusing():
+    print("guess the number")
+    x = int(input("enter a number"))
+    while True:
+        if x == number:
+          print("wow you guessd it")
+numberqusing()
