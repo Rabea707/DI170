226255263f81733de4a461aaffabf775c5f2dc0b
Rabea Sayegh <rabea@example.com>
2026-01-28 00:00:47 +0200

    week 7

--- test1.py
+++ test1.py
@@ -1,8 +1,3 @@
-number = 20
-def numberqusing():
-    print("guess the number")
-    x = int(input("enter a number"))
-    while True:
-        if x == number:
-          print("wow you guessd it")
-numberqusing()
+x = 10
+x += 10
+print(x)
--- week7/day1/script.js
+++ week7/day1/script.js
@@ -1,2 +1,3 @@
-let x = 3
-console.log(x)
+let x = 10
+x += 10
+console.log(x)  
